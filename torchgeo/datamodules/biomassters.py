c6887578933686a2c355fe50efc62d121656a06f
robin.cole@earthdaily.com <robmarkcole@gmail.com>
2026-09-24 07:55:56 +0100

    Add augmentation support to BioMasstersDataModule using Kornia

--- torchgeo/datamodules/biomassters.py
+++ torchgeo/datamodules/biomassters.py
@@ -5,6 +5,7 @@
 
 from typing import Any
 
+import kornia.augmentation as K
 import torch
 from torch.utils.data import random_split
 
@@ -24,6 +25,9 @@ class BioMasstersDataModule(NonGeoDataModule):
     # The task uses these to denormalize predictions; masks remain unnormalized.
     target_mean = 0.0
     target_std = 1.0
+    # Training statistics published by https://github.com/quqixun/BioMassters.
+    s1_mean = torch.tensor([-11.440976, -18.056156, -12.975023, -24.132893])
+    s1_std = torch.tensor([3.167056, 4.362354, 5.392603, 17.264702])
 
     def __init__(
         self,
@@ -44,7 +48,17 @@ class BioMasstersDataModule(NonGeoDataModule):
             BioMassters, batch_size, num_workers, as_time_series=True, **kwargs
         )
         self.lengths = (1 - val_split_pct, val_split_pct)
-        self.aug = torch.nn.Identity()
+
+        sensors = kwargs.get('sensors', BioMassters.valid_sensors)
+        means = {'S1': self.s1_mean, 'S2': torch.zeros(11)}
+        stds = {'S1': self.s1_std, 'S2': torch.ones(11)}
+        mean = torch.cat([means[sensor] for sensor in sensors])
+        std = torch.cat([stds[sensor] for sensor in sensors])
+        self.aug = K.AugmentationSequential(
+            K.VideoSequential(K.Normalize(mean=mean, std=std)),
+            data_keys=None,
+            keepdim=True,
+        )
 
     def setup(self, stage: str) -> None:
         """Set up datasets.
